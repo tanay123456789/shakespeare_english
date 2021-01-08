1555de20f0c7e36350b383d3b83c9d93f58e9e90
tanay123456789 <tanaydwivedi456@gmail.com>
2021-01-08 23:47:28 +0530

    feat:added js file

---
+++ main/app.js
@@ -0,0 +1,33 @@
+var btnTransalate=document.querySelector("#btn-transalate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server!try again after some time")
+}
+
+function clickHandler(){
+    var inputText=txtInput.value;
+    
+    //calling server
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then(json=>{
+        var translatedtext=json.contents.translated;
+        outputDiv.innerText=translatedtext;
+
+    })
+        
+    .catch(errorHandler)
+
+};
+
+
+btnTransalate.addEventListener("click",clickHandler)
